43f7e5ed6ece4888aa56fd37fe38fb2c68d789fe
simonemurzilli <simone.murzilli@fao.org>
2016-10-17 15:37:40 +0200

    added support for arabic

--- nls/ar/translate.js
+++ nls/ar/translate.js
@@ -1,16 +1,11 @@
 /*global define*/
 define({
     // TODO: not reviewed
-    home: 'Home',
-    browse: 'Browse Data',
-    download: 'Download Data',
-    compare: 'مقارنة البيانات',
-    search_data: 'Search Data',
-    analysis: 'Analysis',
-    mes: 'Methods and Standards',
-    faq: 'التعليمات',
+    home: 'منزل',
     data: 'البيانات',
-    search_placeholder: 'بحث مؤشرا أو السلع',
-    browse_by_country: 'مؤشرات البلاد',
-    definitions: "التعاريف و المعايير"
+    country_indicators: 'المؤشرات القطرية',
+    compare: 'مقارنة البيانات',
+    definitions: "التعاريف والمعايير",
+    faq: 'التعليمات',
+    search_placeholder: 'بحث مؤشرا أو السلع'
 });
