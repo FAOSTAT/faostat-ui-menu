af0c6938c17fd9108330d9513517ee18d56089e4
simonemurzilli <simone.murzilli@fao.org>
2016-10-26 10:00:56 +0200

    added spanish translations

--- nls/es/translate.js
+++ nls/es/translate.js
@@ -4,7 +4,7 @@ define({
     data: 'Datos',
     country_indicators: 'Indicadores por País',
     compare: 'Comparar Datos',
-    definitions_and_standards: "Definitions and Standards",
-    faq: 'FAQ',
-    search_placeholder: 'Search an Indicator or Commodity'
+    definitions_and_standards: "Definiciones y Estándares",
+    faq: 'Preguntas Frecuentes',
+    search_placeholder: 'Buscar un Indicador o un Producto'
 });
